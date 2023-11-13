6c84b9f471f620f5f82541bcfe8ea0c6d006ab96
Melli1234d <58852401+Melli1234d@users.noreply.github.com>
2023-11-13 17:46:28 +0100

    Aufteilen in Komponenten

--- src/App.js
+++ src/App.js
@@ -9,98 +9,86 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 
-  const recipes = [
-	{
-	  id: 1,
-	  name: "Kaiserschmarren",
-	  category: "Dessert",
-	},
-	{
-	  id: 2,
-	  name: "Tomate Mozarella",
-	  category: "Obst",
-	},
-  ]
-
-
-
-function App() {
-
-  //Rezept filtern -> von den Daten recipes: wird nach der Kategorie gefiltert
-  const [filteredRecipes, setFilteredRecipes] = useState(recipes);
-  const [category, setCategory] = useState();
-
-  //Setzt den Filter, durchläuft die Recipes(Daten) als recipe durch und returnt 
-  useEffect(() => {
-	setFilteredRecipes(
-	  recipes.filter((recipe) => {
-		return (
-		  (!category ||
-			(category === "Obst"
-			  ? recipe.category === "Obst"
-			  : recipe.category === "Dessert"))
-		);
-	  })
-	);
-  }, [category]);
-
-  const clearFilters = () => {
-	//Filter wieder auf Null setzen
-	setCategory("");
-  };
-
+const RecipeList = ({ recipes }) => {
 	return (
-		<div className="App">
-			<PrimaryButton>
-				Weiter
-			</PrimaryButton>
-			<SecondaryButton>
-				Weiter
-			</SecondaryButton>
-			<RoundButton>
-				6
-			</RoundButton>
-
-
-	  <div className="category_filter">
-        <button className={`${category === "Obst" ? "selected" : ""}`}
-          		onClick={() => setCategory("Obst")}
-        >
-			<img className="content-center eyecatcher" src={logo} alt=" Icon" height={80} width={80} />
-         	<p className='category-description'>Obst</p>
-        </button>
-        <button className={`${category === "Dessert" ? "selected" : ""}`}
-          		onClick={() => setCategory("Dessert")}
-        >
-			<img className="content-center eyecatcher" src={logo} alt=" Icon" height={80} width={80} />
-          	<p className='category-description'> Dessert</p>
-        </button>
-      </div>
-    	<button onClick={clearFilters}>
-		Filter Löschen
-	  	</button>
-      <ul>
-		//Wenn gefiltert wird
-		{filteredRecipes.map((recipe) => {
-          const { name, category, id } = recipe;
-          return (
-            <li key={id}>
-              <div>
-                Name: <strong>{name}</strong>
-              </div>
-              <div>Category: {category}</div>
-            </li>
-          );
-        })}
-
-		//Wenn nichts passendes gefunden wurde beim filtern
-        {filteredRecipes.length === 0 && (
-          <div>Keine Rezepte gefunden</div>
-        )}
-      </ul>
-		</div>
+	  <ul>
+		{recipes.map((recipe) => {
+		  const { name, category, id } = recipe;
+		  return (
+			<li key={id}>
+			  <div>
+				Name: <strong>{name}</strong>
+			  </div>
+			  <div>Category: {category}</div>
+			</li>
+		  );
+		})}
+	  </ul>
 	);
-}
-
-export default App;
-
+  };
+  
+  const CategoryButton = ({ selectedCategory, category, onClick, logo, description }) => {
+	return (
+	  <button className={`${selectedCategory === category ? "selected" : ""}`} onClick={() => onClick(category)}>
+		<img className="content-center eyecatcher" src={logo} alt="Icon" height={80} width={80} />
+		<p className='category-description'>{description}</p>
+	  </button>
+	);
+  };
+  
+  const App = () => {
+	const recipes = [
+	  {
+		id: 1,
+		name: "Kaiserschmarren",
+		category: "Dessert",
+	  },
+	  {
+		id: 2,
+		name: "Tomate Mozarella",
+		category: "Obst",
+	  },
+	];
+  
+	const [filteredRecipes, setFilteredRecipes] = useState(recipes);
+	const [category, setCategory] = useState('');
+  
+	useEffect(() => {
+	  setFilteredRecipes(
+		recipes.filter((recipe) => !category || (category === "Obst" ? recipe.category === "Obst" : recipe.category === "Dessert"))
+	  );
+	}, [category]);
+  
+	const clearFilters = () => {
+	  setCategory('');
+	};
+  
+	return (
+	  <div className="App">
+		<div className="category_filter">
+		  <CategoryButton
+			selectedCategory={category}
+			category="Obst"
+			onClick={setCategory}
+			logo={logo}
+			description="Obst"
+		  />
+		  <CategoryButton
+			selectedCategory={category}
+			category="Dessert"
+			onClick={setCategory}
+			logo={logo}
+			description="Dessert"
+		  />
+		</div>
+		<button onClick={clearFilters}>Filter Löschen</button>
+		{filteredRecipes.length === 0 ? (
+		  <div>Keine Rezepte gefunden</div>
+		) : (
+		  <RecipeList recipes={filteredRecipes} />
+		)}
+	  </div>
+	);
+  };
+  
+  export default App;
